4be86b37c28e6c5f387c3aac954baa98f3f43b41
King Baron ™ <85443712+Alfa00006@users.noreply.github.com>
2022-02-27 01:09:18 +0300

    Update song.py

--- Yukki/Inline/song.py
+++ Yukki/Inline/song.py
@@ -20,7 +20,7 @@ def song_markup(videoid, duration, user_id, query, query_type):
         ],
         [
             InlineKeyboardButton(
-                text="🗑 Close Search",
+                text="🗑 Aramayı Kapat",
                 callback_data=f"forceclose {query}|{user_id}",
             )
         ],
@@ -32,17 +32,17 @@ def song_download_markup(videoid, user_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text="⬇️ Get Audio",
+                text="⬇️ Ses Al",
                 callback_data=f"gets audio|{videoid}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="⬇️ Get Video",
+                text="⬇️ Video Al",
                 callback_data=f"gets video|{videoid}|{user_id}",
             ),
         ],
         [
             InlineKeyboardButton(
-                text="🗑 Close Menu",
+                text="🗑 Menüyü Kapat",
                 callback_data=f"forceclose {videoid}|{user_id}",
             )
         ],
